e6e05cf7c5930c83d123abeca29c64b62732f012
LittleJoe33 <joewatson94@hotmail.co.uk>
2021-06-01 16:20:43 +0100

    regexp working

--- dummyScript.sh
+++ dummyScript.sh
@@ -3,7 +3,21 @@ Lines=$(cat $File)
 RPM=$1
 
 # FORMAT FOR REGEXP = vcg-iid-simulator-1.75.1-425.el7.x86_64.rpm 
-# FORMAT FOR REGEXP = vcg-iid-simulator-((VERSION)).el7.x86_64.rpm 
+# FORMAT FOR REGEXP = vcg-iid-simulator-((VERSION)).el7.x86_64.rpm
+
+#regex='-(([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+))\-'
+
+regex='-(([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)\-([[:digit:]]+))\.'
+
+[[ "$RPM" =~ $regex ]]
+
+echo "${BASH_REMATCH[1]}"
+echo "${BASH_REMATCH[2]}"
+echo "${BASH_REMATCH[3]}"
+echo "${BASH_REMATCH[4]}"
+echo "${BASH_REMATCH[5]}"
+
+##[[ "$RPM" =~ $regex ]]
 
 truncate -s 0 matches.txt
 truncate -s 0 nonMatches.txt
